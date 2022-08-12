8100fbf7f95b018b923e8832d3370187e64e9608
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-08-12 10:58:29 -0400

    Add Blazor hybrid demo

---
+++ src/BlazorHybridWithTelerikXaml/BlazorHybridWithTelerikXaml/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using BlazorHybridWithTelerikXaml.Data;
+using Telerik.Maui.Controls.Compatibility;
+
+namespace BlazorHybridWithTelerikXaml;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .UseTelerik()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
