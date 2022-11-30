13f7413ece362c3a992f2720d97105e986d1292a
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-11-30 13:01:09 -0500

    Add PDF and webview example

---
+++ src/CustomPdfViewer/CustomPdfViewer/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace CustomPdfViewer;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var filePath = Path.Combine(FileSystem.Current.CacheDirectory, "cache.pdf");
+
+        if (File.Exists(filePath))
+            File.Delete(filePath);
+
+        using (var stream = await FileSystem.OpenAppPackageFileAsync("designer_samples.pdf"))
+        using (var ms = new MemoryStream())
+        {
+            await stream.CopyToAsync(ms);
+
+            await File.WriteAllBytesAsync(filePath, ms.ToArray());
+        }
+
+        WebView1.Source = new UrlWebViewSource()
+        {
+            Url = filePath
+        };
+    }
+}
+
