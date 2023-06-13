59e462b48f9fa0e5bb98a39c7aea2a940476facb
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-06-13 16:08:18 -0400

    Update AndroidReportViewer.cs
    Set Android WebView settings to allow Javascript

--- src/CustomReportViewer/CustomReportViewer/Platforms/Android/AndroidReportViewer.cs
+++ src/CustomReportViewer/CustomReportViewer/Platforms/Android/AndroidReportViewer.cs
@@ -1,5 +1,6 @@
 ﻿using Android.Content;
 using Android.Views;
+using Android.Webkit;
 using AndroidX.CoordinatorLayout.Widget;
 using CustomReportViewer.Controls;
 
@@ -17,6 +18,13 @@ public sealed partial class AndroidReportViewer : CoordinatorLayout
         _virtualView = virtualView;
 
         _webView = new Android.Webkit.WebView(_context);
+        _webView.Settings.UseWideViewPort = true;
+        _webView.Settings.LoadWithOverviewMode = true;
+        _webView.Settings.JavaScriptEnabled = true;
+        _webView.Settings.DefaultZoom = WebSettings.ZoomDensity.Far;
+
+        _webView.VerticalScrollBarEnabled = false;
+        _webView.HorizontalScrollBarEnabled = false;
 
         var relativeLayout = new Android.Widget.RelativeLayout(_context)
         {
@@ -30,97 +38,22 @@ public sealed partial class AndroidReportViewer : CoordinatorLayout
 
         this.AddView(relativeLayout);
         
-        var htmlString = $@"<!DOCTYPE html>
-                            <html xmlns=""http://www.w3.org/1999/xhtml"">
-                                <head>
-                                    <title>Telerik MVC HTML5 Report Viewer</title>
-                                    <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"">
-                                    <meta name=""viewport"" content=""width=device-width, initial-scale=1, maximum-scale=1"" />
-
-                                    <script src=""https://code.jquery.com/jquery-3.5.1.min.js""></script>
-                                    <link href=""https://kendo.cdn.telerik.com/2022.2.802/styles/kendo.common.min.css"" rel=""stylesheet"" />
-                                    <link href=""https://kendo.cdn.telerik.com/2022.2.802/styles/kendo.blueopal.min.css"" rel=""stylesheet"" />
-                                    <script src=""~/api/reports/resources/js/telerikReportViewer""></script>
-
-                                    <style>
-                                        #reportViewer1 {{
-                                            position: absolute;
-                                            left: 5px;
-                                            right: 5px;
-                                            top: 5px;
-                                            bottom: 5px;
-                                            font-family: 'segoe ui', 'ms sans serif';
-                                            overflow: hidden;
-                                        }}
-                                    </style>
-                                </head>
-
-                                <body>
-                                    <div id=""reportViewer1""></div>
-                                    <script>
-                                        $(""#reportViewer1"").telerik_ReportViewer({{
-                                            serviceUrl: ""{virtualView.RestServiceUrl}"",
-                                            reportSource: {{ 
-                                                report: ""{virtualView.ReportName}"",
-                                                parameters: ""{{ 
-                                                   ""CultureID"" : ""en""
-                                                }}""
-                                            }}
-                                        }});
-                                    </script>
-                                </body>
-                            </html>";
-
-        _webView.LoadData(htmlString, "text/html", "UTF-8");
+        LoadReportViewer();
     }
 
     public void UpdateRestServiceUrl(string connection)
     {
-        var htmlString = $@"<!DOCTYPE html>
-                            <html xmlns=""http://www.w3.org/1999/xhtml"">
-                                <head>
-                                    <title>Telerik MVC HTML5 Report Viewer</title>
-                                    <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"">
-                                    <meta name=""viewport"" content=""width=device-width, initial-scale=1, maximum-scale=1"" />
-
-                                    <script src=""https://code.jquery.com/jquery-3.5.1.min.js""></script>
-                                    <link href=""https://kendo.cdn.telerik.com/2022.2.802/styles/kendo.common.min.css"" rel=""stylesheet"" />
-                                    <link href=""https://kendo.cdn.telerik.com/2022.2.802/styles/kendo.blueopal.min.css"" rel=""stylesheet"" />
-                                    <script src=""~/api/reports/resources/js/telerikReportViewer""></script>
-
-                                    <style>
-                                        #reportViewer1 {{
-                                            position: absolute;
-                                            left: 5px;
-                                            right: 5px;
-                                            top: 5px;
-                                            bottom: 5px;
-                                            font-family: 'segoe ui', 'ms sans serif';
-                                            overflow: hidden;
-                                        }}
-                                    </style>
-                                </head>
-
-                                <body>
-                                    <div id=""reportViewer1""></div>
-                                    <script>
-                                        $(""#reportViewer1"").telerik_ReportViewer({{
-                                            serviceUrl: ""{connection}"",
-                                            reportSource: {{ 
-                                                report: ""{_virtualView.ReportName}"",
-                                                parameters: ""{{ 
-                                                   ""CultureID"" : ""en""
-                                                }}""
-                                            }}
-                                        }});
-                                    </script>
-                                </body>
-                            </html>";
-
-        _webView.LoadData(htmlString, "text/html", "UTF-8");
+        this._virtualView.RestServiceUrl = connection;
+        LoadReportViewer();
     }
 
     public void UpdateReportName(string reportName)
+    {
+        this._virtualView.ReportName = reportName;
+        LoadReportViewer();
+    }
+
+    private void LoadReportViewer()
     {
         var htmlString = $@"<!DOCTYPE html>
                             <html xmlns=""http://www.w3.org/1999/xhtml"">
@@ -129,10 +62,10 @@ public sealed partial class AndroidReportViewer : CoordinatorLayout
                                     <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"">
                                     <meta name=""viewport"" content=""width=device-width, initial-scale=1, maximum-scale=1"" />
 
-                                    <script src=""https://code.jquery.com/jquery-3.5.1.min.js""></script>
+                                    <script src=""https://code.jquery.com/jquery-3.5.1.min.js"" type=""application/javascript""></script>
                                     <link href=""https://kendo.cdn.telerik.com/2022.2.802/styles/kendo.common.min.css"" rel=""stylesheet"" />
                                     <link href=""https://kendo.cdn.telerik.com/2022.2.802/styles/kendo.blueopal.min.css"" rel=""stylesheet"" />
-                                    <script src=""~/api/reports/resources/js/telerikReportViewer""></script>
+                                    <script src=""~/api/reports/resources/js/telerikReportViewer"" type=""application/javascript""></script>
 
                                     <style>
                                         #reportViewer1 {{
@@ -153,7 +86,7 @@ public sealed partial class AndroidReportViewer : CoordinatorLayout
                                         $(""#reportViewer1"").telerik_ReportViewer({{
                                             serviceUrl: ""{_virtualView.RestServiceUrl}"",
                                             reportSource: {{ 
-                                                report: ""{reportName}"",
+                                                report: ""{_virtualView.ReportName}"",
                                                 parameters: ""{{ 
                                                    ""CultureID"" : ""en""
                                                 }}""
@@ -163,6 +96,6 @@ public sealed partial class AndroidReportViewer : CoordinatorLayout
                                 </body>
                             </html>";
 
-        _webView.LoadData(htmlString, "text/html", "UTF-8");
+        _webView.LoadDataWithBaseURL(null, htmlString, "text/html", "UTF-8", null);
     }
 }
