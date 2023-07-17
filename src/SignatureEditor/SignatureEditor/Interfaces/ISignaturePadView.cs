fd600783d0527c0d12f4ac8eba1e801f3df4b5ac
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-07-17 17:11:57 -0400

    Add demo

---
+++ src/SignatureEditor/SignatureEditor/Interfaces/ISignaturePadView.cs
@@ -0,0 +1,12 @@
+﻿using Telerik.Maui.Controls.SignaturePad;
+
+namespace SignatureEditor.Interfaces;
+
+public interface ISignaturePadView
+{
+    void ClearSignature();
+
+    Task SaveImageAsync(Stream outputStream);
+
+    Task SaveImageAsync(Stream outputStream, SaveImageSettings settings);
+}
