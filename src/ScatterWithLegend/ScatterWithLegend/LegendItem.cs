6d8373fe0e788294278fe3f3ba7bfabf1fac7671
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-07-19 17:23:35 -0400

    Add selection feature to custom legend

--- src/ScatterWithLegend/ScatterWithLegend/LegendItem.cs
+++ src/ScatterWithLegend/ScatterWithLegend/LegendItem.cs
@@ -1,7 +1,28 @@
+using CommonHelpers.Common;
+
 namespace ScatterWithLegend;
 
-public class LegendItem
+public class LegendItem : BindableBase
 {
-    public Color SeriesColor { get; set; }
-    public string SeriesDisplayName { get; set; }
+    private bool isSelected = true;
+    private string seriesDisplayName;
+    private Color seriesColor;
+    
+    public Color SeriesColor
+    {
+        get => seriesColor;
+        set => SetProperty(ref seriesColor, value);
+    }
+
+    public string SeriesDisplayName
+    {
+        get => seriesDisplayName;
+        set => SetProperty(ref seriesDisplayName, value);
+    }
+
+    public bool IsSelected
+    {
+        get => isSelected;
+        set => SetProperty(ref isSelected, value);
+    }
 }
