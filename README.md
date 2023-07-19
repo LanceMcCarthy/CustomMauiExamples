3f5bfebb9a7d0d52b2d2425e3d3a2b7779449db9
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-07-19 17:31:13 -0400

    Add selection feature to custom legend (#10)
    * Add selection feature to custom legend
    
    * Update image source
    
    * Update README.md

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ This demo shows how to dynamically load signatures and swap between **edit-mode*
 
 If you would like more control over the appearance of your chart legend, create a completely custom legend with minimal effort using a RadItemsControl and the chart's Loaded event. This demo shows you exactly how it's done, visit MainPage to see the code ([Jump to code](src/ScatterWithLegend/)).
 
-![custom chart legend](https://github.com/LanceMcCarthy/CustomMauiExamples/assets/3520532/9f27470c-40a8-4b4c-a549-d90b4c158886)
+![custom chart legend](/images/custom-series-legend.gif)
 
 ## Printing
 
--- src/ScatterWithLegend/ScatterWithLegend/MainPage.xaml.cs
+++ src/ScatterWithLegend/ScatterWithLegend/MainPage.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Telerik.Maui.Controls.Compatibility.Chart;
 
 namespace ScatterWithLegend;
 
 public partial class MainPage : ContentPage
 {
     private readonly ObservableCollection<LegendItem> legendItems = new ();
-
+    private readonly List<CartesianSeries> seriesCache = new();
+    
     public MainPage()
     {
         InitializeComponent();
@@ -28,8 +31,69 @@ public partial class MainPage : ContentPage
             this.legendItems.Add(new LegendItem
             {
                 SeriesColor = paletteColor,
-                SeriesDisplayName = displayName
+                SeriesDisplayName = displayName,
+                IsSelected = true
             });
+
+            // Important: SETUP the cache now, this maintains the correct index
+            seriesCache.Add(series);
         }
     }
+
+    private void CheckBox_OnCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (((CheckBox)sender).BindingContext is not LegendItem legendItem) 
+            return;
+
+        // get a reference to the series in the cache that matches the legend item
+        var series = seriesCache.FirstOrDefault(s => s.DisplayName == legendItem.SeriesDisplayName);
+
+        // CONDITION 1 - Add the series back to the chart
+        if (legendItem.IsSelected && series != null) 
+        {
+            // look in the cache for the series
+            var seriesIndex = seriesCache.IndexOf(series);
+
+            // CheckChanged is going to fire when the initial value is bound, ignore this cycle
+            if (chartTest.Series.Contains(series)) 
+                return; 
+
+            // add it back to the chart at the correct index
+            chartTest.Series.Insert(seriesIndex, series);
+            
+            // Set the item's color to match the series's palette item
+            SyncPaletteColors();
+        }
+        // CONDITION 2 - Remove the series from the chart
+        else if (!legendItem.IsSelected && series != null)
+        {
+            // remove the series from the chart (the reference is maintained in memory because it is in the cache)
+            chartTest.Series.Remove(series);
+
+            // Unset the color so that the palette-assigned color is not out of sync, then update the other checkboxes colors
+            legendItem.SeriesColor = Colors.Gray;
+            SyncPaletteColors();
+        }
+        else
+        {
+            Debug.WriteLine("Executed too early, nothing to see here... move on.");
+        }
+    }
+    
+    private void SyncPaletteColors()
+    {
+        foreach (var chartSeries in chartTest.Series)
+        {
+            var matchingLegendItem = legendItems.FirstOrDefault(item => item.SeriesDisplayName == chartSeries.DisplayName);
+
+            var seriesIndex = chartTest.Series.IndexOf(chartSeries);
+
+            if (matchingLegendItem != null)
+            {
+                matchingLegendItem.SeriesColor = chartTest.Palette.Entries[seriesIndex].FillColor;
+            }
+        }
+    }
+
+    
 }
