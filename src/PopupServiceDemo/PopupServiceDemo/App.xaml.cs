593a32511048760a9309fe75ad4040eef7cbb0cc
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-08-02 11:56:22 -0400

    Add popup dependency injection service demo (#12)

---
+++ src/PopupServiceDemo/PopupServiceDemo/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Telerik.Maui.Controls;
+
+namespace PopupServiceDemo;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        this.MainPage = new AppShell();
+    }
+}
---
+++ src/PopupServiceDemo/PopupServiceDemo/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using PopupServiceDemo.Views;
+
+namespace PopupServiceDemo;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage(HomeView view, PopupService service)
+    {
+        InitializeComponent();
+        this.Content = view;
+        
+        service.AttachParent(this);
+    }
+}
