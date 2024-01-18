458a0244bfcd2926de2bb208d302a9d5bfd732f1
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-01-18 10:11:43 -0500

    Project created

---
+++ src/ChatDataSources/ChatDataSources.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatDataSources.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
